e98dcd6250bb9060b121a135ae620dd9dbef0201
SaurabhSven <srbhmhatre07@gmail.com>
2026-02-05 14:55:18 +0530

    Changes in Initial commit

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -3,7 +3,6 @@ import ProductImages from "@/components/shared/product/product-images";
 import ProductPrice from "@/components/shared/product/product-price";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import { Product } from "@/types";
 import { notFound } from "next/navigation";
