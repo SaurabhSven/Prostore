84cd6cc32a3bc1aaa6125670cdaa9cd7402fb4e7
SaurabhSven <srbhmhatre07@gmail.com>
2026-04-16 13:22:20 +0530

    Changes in next config file

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.0.124'],
+  // allowedDevOrigins: ['192.168.0.124'],
   images:{
     remotePatterns:[
       {
